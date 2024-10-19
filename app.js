00dc0ec6be7fd6e18b561930a34adf7ba8e9d090
Deepak <deepak@Deepaks-MacBook-Air.local>
2024-10-20 05:12:59 +0530

    rfegter

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 app.get("/", (req, res) => {
   return res.status(200).json({
-    data: "workinjefg",
+    data: "workinjefg rei0rii",
   });
 });
 app.listen(5555, () => {
